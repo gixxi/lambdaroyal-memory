364d94c0a5c084b09596715b5d4644636a01ffa1
gixxi <christian.meichsner@WS033.xelog.ch>
2015-02-10 16:18:59 +0100

    removed unused dirs

--- src/lambdaroyal/memory/context/factory.clj
+++
@@ -1,9 +0,0 @@
-(ns lambdaroyal.memory.context.factory
-    (:gen-class))
-
-(defn- create-collection [collection]
-  (ref {:data (ref (sorted-map))
-        :constraints (ref {})}))
-
-(defn create-context [meta-model]
-  (zipmap (keys meta-model) (map #(create-collection %) (vals meta-model))))
